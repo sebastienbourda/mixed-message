134bd7238063ef89b02ac412ab474019d47e9df3
Sebastien Bourda <32631203+sebastienbourda@users.noreply.github.com>
2021-05-22 14:47:21 +0200

    remove white colored background

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const animals = ["Bird", "Dog", "Cat", "Lion", "Rat", "Tiger", "Crocodile", "Gol
 const links = ["is in", "is at", "is on", "is under", "is below", "is next to", "is beside", "is over", "is above", "is accros", "is on top of", "is on the corner of", "is next to","is between","is at the end of","is in front of","is on the left of","is on the right of","is behind", "go trough", "go to", "go into", "go towards", "go onto", "went from"];
 const locations = ["the branch", "the kitchen", "the book", "the living room", "the toilet", "the tree", "its nest"];
 
-const colors = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'grey', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
+const colors = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'grey', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'yellow'];
 
 const createSentence = () => {
 	let animal = animals[Math.floor(Math.random() * animals.length)];
@@ -10,6 +10,14 @@ const createSentence = () => {
 	let location = locations[Math.floor(Math.random() * locations.length)];
 	let sentenceContainer = document.getElementById("sentence-container");
 	sentenceContainer.innerHTML += `<h1 class="sentence">The ${animal} ${link} ${location}.<br>I repeat, the ${animal} ${link} ${location}.*</h1>`;
+}
+
+const changeBackgroundColor = () => {
+	const mainContainer = document.getElementById("sentence-container");
+	const instruction = document.getElementById("instruction");
+	const color = colors[Math.floor(Math.random() * colors.length)];
+	mainContainer.style.backgroundColor = color;
+	instruction.style.backgroundColor = color;
 }
 
 const resetAndCreateSentence = () => {
@@ -21,20 +29,6 @@ const resetAndCreateSentence = () => {
 	changeBackgroundColor();
 }
 
-const changeBackgroundColor = () => {
-	const mainContainer = document.getElementById("sentence-container");
-	const footer = document.getElementById("pressakey");
-	const color = colors[Math.floor(Math.random() * colors.length)];
-	if(color=='white'){
-		mainContainer.style.color = 'black';	
-		pressakey.style.color = 'black';
-	}else if (color=='black'){
-		mainContainer.style.color = 'white';	
-		pressakey.style.color = 'white';
-	}
-	mainContainer.style.backgroundColor = color;
-	pressakey.style.backgroundColor = color;
-}
 
 window.onload = () => {
 	createSentence();
